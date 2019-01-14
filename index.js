5a02f5ff3a4891fb1ab09e516a03c31f4a7dff5c
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-14 21:58:05 +0700

    Excercise Prototype Stopwatch

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-function Circle(radius) {
-    //instance Members
-    this.radius = radius
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0
+
+    Object.defineProperty(this, 'duration', {
+        get: function () { return duration },
+        set: function (value) { duration = value }
+    })
 }
-Circle.prototype.draw = function () {
-    //Prototype Members
-    console.log('draw')
-}   //No repeat always
-
-const c1 = new Circle(1)
-
-Circle.prototype.toString = function () {
-    return 'Circle with Radius ' + this.radius
+//This Is bad Idea
+Stopwatch.prototype.start = function () {
+    if (this.running)
+        throw new Error('Sedang Berjalan')
+    this.running = true
+    this.startTime = new Date()
 }
-
-//Only Instance Member
-console.log(Object.keys(c1))
-
-//all member instace n prototype
-for (let key in c1) console.log(key)
-
-//Only Instace Property
-console.log(c1.hasOwnProperty('draw'))
+Stopwatch.prototype.stop = function () {
+    if (!this.running)
+        throw new Error('Sedang tidak Berjalan Berjalan')
+    this.running = false
+    this.endTime = new Date()
+    const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000
+    this.duration += seconds
+}
+Stopwatch.prototype.reset = function () {
+    this.startTime = null
+    this.endTime = null
+    this.running = false
+    this.duration = 0
+}
