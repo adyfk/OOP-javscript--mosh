4cc1b7202324acade7b254f9b6ef1c58e769748f
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-15 10:24:43 +0700

    Calling the Super Constructor

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-function Shape() {
-
+function Shape(color) {
+    this.color = color
 }
 Shape.prototype.duplicate = function () {
     console.log('duplicate')
 }
-function Circle(radius) {
+function Circle(radius, color) {
+    // Shape(color)     //window.color
+    Shape.call(this, color)
+
     this.tadius = radius
 }
 Circle.prototype = Object.create(Shape.prototype)
 Circle.prototype.constructor = Circle
-const c = new Circle(10)
+const c = new Circle(10, 'red')
