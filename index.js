f9a0f492e12429e0b3dc38fe861e07809c78d3c7
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-15 23:36:42 +0700

    Method Overriding

--- index.js
+++ index.js
@@ -2,26 +2,17 @@ function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 }
-function Shape(color) {
-  this.color = color;
-}
-Shape.prototype.duplicate = function() {
-  console.log("duplicate");
+function Shape() { }
+Shape.prototype.duplicate = function () {
+  console.log("Duplicate Shape");
 };
 
-function Circle(radius, color) {
-  Shape.call(this, color);
-  this.radius = radius;
-}
-extend(Circle, Shape); //Jikaextend setelah create prototype , will be replace
-Circle.prototype.draw = function() {
-  console.log("draw");
+function Circle() { }
+extend(Circle, Shape);
+
+Circle.prototype.duplicate = function () {
+  Shape.prototype.duplicate.call(this)
+  console.log("Duplicate Circle");
 };
 
-function Square(size) {
-  this.size = size;
-}
-extend(Square, Shape);
-
-const c = new Circle(10, "red");
-const s = new Square(12);
+const c = new Circle();
