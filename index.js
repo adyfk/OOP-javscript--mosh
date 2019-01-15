23ff43126cbe7ba2688c9b8c8b37c3422fd7953a
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-15 10:01:01 +0700

    Create Your Own Ptototypica Inheritance

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-function Stopwatch() {
-    let startTime, endTime, running, duration = 0
+function Shape() {
 
-    Object.defineProperty(this, 'duration', {
-        get: function () { return duration },
-        set: function (value) { duration = value }
-    })
 }
-//This Is bad Idea
-Stopwatch.prototype.start = function () {
-    if (this.running)
-        throw new Error('Sedang Berjalan')
-    this.running = true
-    this.startTime = new Date()
+Shape.prototype.duplicate = function () {
+    console.log('duplicate')
 }
-Stopwatch.prototype.stop = function () {
-    if (!this.running)
-        throw new Error('Sedang tidak Berjalan Berjalan')
-    this.running = false
-    this.endTime = new Date()
-    const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000
-    this.duration += seconds
+function Circle(radius) {
+    this.tadius = radius
 }
-Stopwatch.prototype.reset = function () {
-    this.startTime = null
-    this.endTime = null
-    this.running = false
-    this.duration = 0
-}
+Circle.prototype = Object.create(Shape.prototype)
+
+const c = new Circle(10)
