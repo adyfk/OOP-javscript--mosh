2a4362d0912d88494a1fa95d771eeba274d8df6e
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-15 23:47:37 +0700

    Polymorphism

--- index.js
+++ index.js
@@ -11,8 +11,30 @@ function Circle() { }
 extend(Circle, Shape);
 
 Circle.prototype.duplicate = function () {
-  Shape.prototype.duplicate.call(this)
   console.log("Duplicate Circle");
 };
 
-const c = new Circle();
+function Square() { }
+extend(Square, Shape);
+
+Square.prototype.duplicate = function () {
+  console.log("Duplicate Square");
+};
+
+const Shapes = [
+  new Circle(),
+  new Square()
+]
+
+// for (let Shape of Shapes) {
+//   if (Shape.type === 'Circle')
+//     duplicateCircle()
+//   else if (Shape.type === 'Square')
+//     duplicateSquare()
+//   else
+//     duplicateShape()
+// }
+
+for (let Shape of Shapes) {
+  Shape.duplicate() //This is Polymorphism
+} 
