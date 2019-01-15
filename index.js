09843d3face812aec6fa750b5a8aba00a6f98c25
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-15 21:32:01 +0700

    Intermediate Function Inheritance

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
 function Shape(color) {
-    this.color = color
+  this.color = color;
 }
-Shape.prototype.duplicate = function () {
-    console.log('duplicate')
-}
-function Circle(radius, color) {
-    // Shape(color)     //window.color
-    Shape.call(this, color)
+Shape.prototype.duplicate = function() {
+  console.log("duplicate");
+};
 
-    this.tadius = radius
+function Circle(radius, color) {
+  Shape.call(this, color);
+  this.radius = radius;
 }
-Circle.prototype = Object.create(Shape.prototype)
-Circle.prototype.constructor = Circle
-const c = new Circle(10, 'red')
+extend(Circle, Shape); //Jikaextend setelah create prototype , will be replace
+Circle.prototype.draw = function() {
+  console.log("draw");
+};
+
+function Square(size) {
+  this.size = size;
+}
+extend(Square, Shape);
+
+const c = new Circle(10, "red");
+const s = new Square(12);
