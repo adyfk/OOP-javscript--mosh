a9e54748d508ffbf331e77796b5a6d66e7b8d75e
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-16 13:29:43 +0700

    ES6 Classes

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-function HtmlELement() {
-  this.click = function () {
-    console.log('Click')
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+    this.move = function () { }
+  }
+  draw() {
+    console.log('Draw')
   }
 }
-HtmlELement.prototype.focus = function () {
-  console.log('Focued')
-}
-
-function HtmlSelectELement(items = []) {
-  this.items = items
-
-  this.addItem = function (item) {
-    this.items.push(item)
-  }
-
-  this.removeItem = function (item) {
-    this.items.splice(this.items.indexOf(item), 1)
-  }
-
-  this.render = function () {
-    return `
-    <select>
-      ${this.items.map(i => `<option>${i}</option>`).join('')}
-    </select>
-    `
-  }
-}
-// HtmlSelectELement.prototype = Object.create(HtmlELement.prototype)//Klik Not Include
-HtmlSelectELement.prototype = new HtmlELement() //Solution
-HtmlSelectELement.prototype.constructor = HtmlSelectELement()
-
-function HtmlImageElement(src) {
-  this.src = src
-  this.render = function () {
-    return `<img src="${this.src}" />`
-  }
-}
-
-HtmlImageElement.prototype = new HtmlELement()
-HtmlImageElement.prototype.constructor = HtmlImageElement()
+const c = new Circle(10)
