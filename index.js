b85b98d3f502edde1f5947e205e6fe2038d693b8
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-16 15:58:23 +0700

    Private Members Using Symbol

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-//
-//By default in the body on classes (use Strict) Mode 
-//Prevent The Global Object
-//
+const _radius = Symbol()
+const _draw = Symbol()
+
+class Circle {
+    constructor(radius) {
+        this[_radius] = radius
+    }
+    [_draw]() {
+
+    }
+}
+
+//check any or not //it will not any
+const c = new Circle(10)
+console.log('This is C', c)
+console.log(c)
+console.log(Object.getOwnPropertySymbols(c))
