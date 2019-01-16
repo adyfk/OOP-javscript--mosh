12796d3ce0887c2ca48852c5b8cb6447e2563f5b
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-16 16:55:35 +0700

    Solution Excercise Stack

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-class Shape {
-    move() {
-        console.log('Shape Move')
+const _items = new WeakMap()
+class Stack {
+    constructor() {
+        _items.set(this, [])
     }
-}
-class Circle extends Shape {
-    move() {
-        super.move()
-        console.log('Circle Move')
+    push(obj) {
+        _items.get(this).push(obj)
+    }
+    pop() {
+        const items = _items.get(this)
+        if (items.length === 0)
+            throw new Error('Data Kosong')
+        return items.pop()
+    }
+    peek() {
+        const items = _items.get(this)
+        if (items.length === 0)
+            throw new Error('Data Kosong')
+        return items[items.length - 1]
+    }
+    count() {
+        return _items.get(this).length
     }
 }
 
-const c = new Circle('red', 10)
-console.log(c)
